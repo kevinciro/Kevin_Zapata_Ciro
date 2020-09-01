32e67b2f35ae3fab4a5c1ad1a91608e29b5d45d2
kevinciro <70342180+kevinciro@users.noreply.github.com>
2020-08-31 22:07:10 -0500

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -1,1 +1,14 @@
-
+def Factorial( int ):
+    # Funcion para calcular el factorial de un entero dado
+    
+    n = 1 #Variable "muda" para guardar en valor del factorial
+    
+    #ciclo para calcular el factorial
+    for i in range(1, int+1):
+        #cada iteracion se le suma 1 a i, empieza en 1, y se multiplica por el valor anterior de n
+        n = n*i
+        
+    return n
+  
+  
+  
