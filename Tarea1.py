d8456c43a1cc4eb5b8d4250f28204b717b31c628
kevinciro <70342180+kevinciro@users.noreply.github.com>
2020-09-01 15:34:21 -0500

    Update Tarea1.py

--- Tarea1.py
+++ Tarea1.py
@@ -12,6 +12,6 @@ print("La probabilidad de obtener 10 caras en 100 intentos es: ", prob)
 #la probabilidad de obtener mas de 30 caras en 100 intentos
 n=100
 prob1=0
-for k in range(30,101):
+for k in range(31,101):
     prob1 = prob1 + ( funciones.Binomial(n,k)/(2**(n)) )
 print("La probabilidad de obtener mas de 30 caras en 100 intentos es: ", prob1)
