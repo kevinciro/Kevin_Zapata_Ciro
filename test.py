7edb368f0675b4320d34ba668c0378399dbd2698
kevinciro <70342180+kevinciro@users.noreply.github.com>
2020-09-03 13:26:05 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
 import funciones
+
+#el factorial de 5 es 120
+print(funciones.Factorial(5))
+
+# si se pasa un decimal (float) tira error
+print(funciones.Factorial(0.3))
+
+#el factor binomial de 4,2 es 6
+print(funciones.Binomial(4,2))
+
+#si se le pasa un float tira error
+print(funciones.Binomial(4,2.))
